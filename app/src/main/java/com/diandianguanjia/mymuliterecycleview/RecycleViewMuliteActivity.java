41c5f3a09751b4179e1fa1bbf8eca60def66fe19
破荒 <13531829360@163.com>
2017-09-16 08:05:11 +0800

    RecycleView多布局实现

--- app/src/main/java/com/diandianguanjia/mymuliterecycleview/RecycleViewMuliteActivity.java
+++ app/src/main/java/com/diandianguanjia/mymuliterecycleview/RecycleViewMuliteActivity.java
@@ -19,7 +19,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class RecycleViewMuliteActivity extends AppCompatActivity implements MyAdapter.OnItemClickListener {
+public class  RecycleViewMuliteActivity extends AppCompatActivity implements MyAdapter.OnItemClickListener {
 
     private String []titles={"asejhf","skdlrf","sekcjfwakejdre","sferbgerf","dfsrftg"};
     private static final String TAG = "RecycleViewMuliteActivi";
